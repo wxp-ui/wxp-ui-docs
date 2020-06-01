0ac9574564bf40045a8cbe09a28a50a7e42fc0f6
singletouch <singletouch@163.com>
2020-06-01 08:55:10 +0800

    fix

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'WxP UI Test', // Title for your website.
-  tagline: 'WxP UI 是一款提供高交互小程序插件的合集, 致力于简洁和高可用性的插件实现',
+  tagline: 'WxP UI 是一款提供高交互小程序插件的合集, 致力于简洁和高可用性的插件实现 1212',
   url: 'https://wxp-ui.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
